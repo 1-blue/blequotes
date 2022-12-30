be42947f29f1ddf6cc0a78995f3bd03ba1b8f478
1-blue <1-blue98@naver.com>
2022-12-30 18:55:08 +0900

    Add: post rendering logic
    1. 드라마 게시글 렌더링 로직 작성
    2. 도서 게식르 렌더링 로직 작성

---
+++ frontend/src/components/Page/Book.tsx
@@ -0,0 +1,90 @@
+import { useState, useEffect, useCallback } from "react";
+import { useAppDispatch, useAppSelector } from "@src/hooks/useRTK";
+import { postThunkService } from "@src/store/thunks";
+import { postActions } from "@src/store/reducers/postReducer";
+
+// hook
+import useInfiniteScrolling from "@src/hooks/useInfiniteScrolling";
+
+// components
+import GridPosts from "@src/components/Posts/GridPosts";
+import PostHeader from "@src/components/Posts/PostHeader";
+
+// type
+import type { PostSortBy } from "@src/types";
+
+const Book = () => {
+  const dispatch = useAppDispatch();
+  const { bookPosts, hasMoreBookPosts, getPostsLoading } = useAppSelector(
+    ({ post }) => post
+  );
+
+  // 2022/12/30 - 게시글들 정렬 순서 - by 1-blue
+  const [sortBy, setSortBy] = useState<PostSortBy>("popular");
+
+  // 2022/12/30 - 도서 게시글들 패치 - by 1-blue
+  useEffect(() => {
+    dispatch(postActions.reset());
+
+    dispatch(
+      postThunkService.getPostsThunk({
+        category: "BOOK",
+        sortBy,
+        take: 20,
+        lastId: -1,
+      })
+    );
+  }, [dispatch, sortBy]);
+
+  // 2022/12/30 - 무한 스크롤링을 위해 관찰할 태그 ref ( 해당 태그가 뷰포트에 들어오면 게시글 추가 패치 실행 ) - by 1-blue
+  // ( ref지만 값에 의해 렌더링에 영향을 끼지기 때문에 "useState()""사용 )
+  const [observerRef, setObserverRef] = useState<null | HTMLDivElement>(null);
+  // 2022/12/30 - 도서 더 가져오기 - by 1-blue
+  const fetchMoreBooks = useCallback(() => {
+    if (getPostsLoading) return;
+    if (!hasMoreBookPosts) return;
+
+    dispatch(
+      postThunkService.getPostsThunk({
+        category: "BOOK",
+        sortBy,
+        take: 20,
+        lastId: bookPosts[bookPosts.length - 1].id,
+      })
+    );
+  }, [getPostsLoading, hasMoreBookPosts, dispatch, sortBy, bookPosts]);
+  // 2022/12/30 - 도서 가져오기 무한 스크롤링 적용 - by 1-blue
+  useInfiniteScrolling({
+    observerRef,
+    fetchMore: fetchMoreBooks,
+    hasMore: hasMoreBookPosts,
+  });
+
+  // 2022/12/30 - 게시글들 정렬 순서 변경 - by 1-blue
+  const onChangeSortBy = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setSortBy(e.target.value as PostSortBy);
+    },
+    []
+  );
+
+  return (
+    <>
+      {/* 네비게이션 바의 높이가 92px이라서 상단을 띄워주기 위해 사용 */}
+      <section className="h-[92px] bg-gray-600 mb-2"></section>
+
+      {/* 게시글들 */}
+      <section className="mx-4">
+        <PostHeader
+          title="도서 명대사들"
+          onChangeSortBy={onChangeSortBy}
+          sortBy={sortBy}
+        />
+
+        <GridPosts posts={bookPosts} ref={setObserverRef} />
+      </section>
+    </>
+  );
+};
+
+export default Book;
--- frontend/src/components/Page/Drama.tsx
+++ frontend/src/components/Page/Drama.tsx
@@ -1,23 +1,49 @@
-import { useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, useCallback } from "react";
 import { useAppDispatch, useAppSelector } from "@src/hooks/useRTK";
-import { dramaThunkService } from "@src/store/thunks";
+import { dramaThunkService, postThunkService } from "@src/store/thunks";
+import { postActions } from "@src/store/reducers/postReducer";
 
 // util
 import { getMovieDBImagePath } from "@src/utils";
+
+// hook
+import useInfiniteScrolling from "@src/hooks/useInfiniteScrolling";
 
 // component
 import Image from "@src/components/Common/Image";
 import SlickSlider from "@src/components/Common/SlickSlider";
 import SkeletonUI from "@src/components/Common/SkeletonUI";
+import PostHeader from "@src/components/Posts/PostHeader";
+import GridPosts from "@src/components/Posts/GridPosts";
 
 // type
-import type { PostCategory } from "@src/types";
+import type { PostCategory, PostSortBy } from "@src/types";
 
 const Drama = () => {
   const dispatch = useAppDispatch();
   const { popular, top_rated, on_the_air, fetchDramasLoading } = useAppSelector(
     ({ drama }) => drama
   );
+  const { dramaPosts, hasMoreDramaPosts, getPostsLoading } = useAppSelector(
+    ({ post }) => post
+  );
+
+  // 2022/12/30 - 게시글들 정렬 순서 - by 1-blue
+  const [sortBy, setSortBy] = useState<PostSortBy>("popular");
+
+  // 2022/12/30 - 드라마 게시글들 패치 - by 1-blue
+  useEffect(() => {
+    dispatch(postActions.reset());
+
+    dispatch(
+      postThunkService.getPostsThunk({
+        category: "DRAMA",
+        sortBy,
+        take: 20,
+        lastId: -1,
+      })
+    );
+  }, [dispatch, sortBy]);
 
   // 2022/12/15 - 각종 드라마들 패치 - by 1-blue
   useEffect(() => {
@@ -31,6 +57,31 @@ const Drama = () => {
       dispatch(dramaThunkService.fetchDramasThunk({ category: "on_the_air" }));
     }
   }, [dispatch, popular, top_rated, on_the_air]);
+
+  // 2022/12/30 - 무한 스크롤링을 위해 관찰할 태그 ref ( 해당 태그가 뷰포트에 들어오면 게시글 추가 패치 실행 ) - by 1-blue
+  // ( ref지만 값에 의해 렌더링에 영향을 끼지기 때문에 "useState()""사용 )
+  const [observerRef, setObserverRef] = useState<null | HTMLDivElement>(null);
+  // 2022/12/30 - 드라마 더 가져오기 - by 1-blue
+  const fetchMoreDrama = useCallback(() => {
+    if (getPostsLoading) return;
+    if (!hasMoreDramaPosts) return;
+
+    dispatch(
+      postThunkService.getPostsThunk({
+        category: "DRAMA",
+        sortBy,
+        take: 20,
+        lastId: dramaPosts[dramaPosts.length - 1].id,
+      })
+    );
+  }, [getPostsLoading, hasMoreDramaPosts, dispatch, sortBy, dramaPosts]);
+  // 2022/12/30 - 드라마 가져오기 무한 스크롤링 적용 - by 1-blue
+  useInfiniteScrolling({
+    observerRef,
+    fetchMore: fetchMoreDrama,
+    hasMore: hasMoreDramaPosts,
+  });
+
   // 2022/12/15 - 인기 / 현재 상영중 / 꾸준한 인기 드라마들 필터링 ( 현재 브라우저 사이즈에 맞는 이미지 없는 경우 제외 ) - by 1-blue
   const filteredPopularDatas = useMemo(
     () =>
@@ -108,6 +159,14 @@ const Drama = () => {
     [top_rated]
   );
 
+  // 2022/12/30 - 게시글들 정렬 순서 변경 - by 1-blue
+  const onChangeSortBy = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setSortBy(e.target.value as PostSortBy);
+    },
+    []
+  );
+
   // 드라마를 패치하는 중이라면
   if (popular.length === 0 || top_rated.length === 0 || on_the_air.length === 0)
     return <SkeletonUI.Page />;
@@ -165,6 +224,17 @@ const Drama = () => {
           <div className="py-6" />
         </section>
       )}
+
+      {/* 게시글들 */}
+      <section className="mx-4">
+        <PostHeader
+          title="드라마 명대사들"
+          onChangeSortBy={onChangeSortBy}
+          sortBy={sortBy}
+        />
+
+        <GridPosts posts={dramaPosts} ref={setObserverRef} />
+      </section>
     </>
   );
 };
